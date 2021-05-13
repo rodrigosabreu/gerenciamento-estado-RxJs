d9cd57c1876a37c8e2d133cbe7a0e2fdcceb3dde
Rodrigo Abreu <rodrigo.s.abreu@gmail.com>
2021-05-13 11:48:57 -0300

    gerenciamento de estado com RxJS

---
+++ src/app/demos/todo-list/components/todo-list/todo-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from "../../task";
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['todo-list.component.css']  
+})
+export class ToDoListComponent {
+
+  @Input()//inputar eventos
+  list: Task[];
+
+  @Output()//sair eventos
+  toggle = new EventEmitter<any>();
+
+  toggleItem(index: number, acao: string){
+
+    const task = this.list[index];
+    
+    switch (acao) {
+      case 'iniciar':
+        task.finalizado = false;
+        task.iniciado = true;
+        break; 
+      case 'finalizar':
+        task.finalizado = true;
+        task.iniciado = false;
+        break; 
+      case 'retomar':
+        task.finalizado = false;
+        task.iniciado = true;
+        break; 
+      case 'cancelar':
+        task.finalizado = false;
+        task.iniciado = false;
+        break;      
+    }
+
+    this.toggle.emit({
+      task: { ...task }
+    });
+
+  }
+
+}
